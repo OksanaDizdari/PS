ddb9a553c3e5fe67bbd1a0b12a2caf426b20d18c
OksanaDizdari <Oksana.Dizdari@gmail.com>
2015-03-26 20:00:11 +0000

    SQL script to create table user and device

---
+++ ServerSide_PushNotificationsWithGCM/DatabaseScript/CreateTables.sql
@@ -0,0 +1,20 @@
+﻿DROP TABLE IF EXISTS _device;
+DROP TABLE IF EXISTS _user;
+
+CREATE TABLE _user
+(
+  _name character varying(20) NOT NULL,
+  _password character varying(140) NOT NULL,
+  CONSTRAINT _user_pkey PRIMARY KEY (_name)
+);
+
+
+CREATE TABLE _device
+(
+  _key integer NOT NULL,
+  _user character(20),
+  CONSTRAINT _device_pkey PRIMARY KEY (_key),
+  CONSTRAINT _user FOREIGN KEY (_user)
+      REFERENCES _user (_name) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
