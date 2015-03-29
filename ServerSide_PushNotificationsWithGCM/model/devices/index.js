4ba664349f4563b80526d74593f3bb92e3c23f09
OksanaDizdari <Oksana.Dizdari@gmail.com>
2015-03-29 17:27:09 +0100

    Orgnized device's methods.

--- ServerSide_PushNotificationsWithGCM/model/devices/index.js
+++ ServerSide_PushNotificationsWithGCM/model/devices/index.js
@@ -1,3 +1,57 @@
 /**
- * Created by beatr_000 on 28/03/2015.
+ * Created by Beatriz and Oksana on 28/03/2015.
  */
+
+var db = require("./../../model");
+
+function insertDevice(user, key, client, fn){
+    db.pg.connect(db.conString, function(err, client, done) {
+
+        if(err) {
+            console.log(err);
+            done();
+            return fn(err);
+        }
+
+        client.query("INSERT INTO _device (_key, _user, _client) VALUES($1, $2, $3)", [user, key, client],
+            function(err)
+            {
+                if(err) {
+                    console.log(err);
+                    done();
+                    return fn(err);
+                }
+                done();
+                return fn(null);
+            }
+        );
+    });
+}
+
+function deleteDevice(key,fn) {
+
+    db.pg.connect(db.conString, function(err, client, done) {
+
+        if(err) {
+            console.log(err);
+            done();
+            return fn(err);
+        }
+
+        client.query("DELETE FROM _device WHERE _key=$1", [key],
+            function(err)
+            {
+                if(err) {
+                    console.log(err);
+                    done();
+                    return fn(err);
+                }
+                done();
+                return fn(null);
+            }
+        );
+    });
+}
+
+module.exports.insertDevice = insertDevice;
+module.exports.deleteDevice = deleteDevice;
--- ServerSide_PushNotificationsWithGCM/model/index.js
+++ ServerSide_PushNotificationsWithGCM/model/index.js
@@ -8,63 +8,10 @@ var conString = fs.readFileSync("config.properties").toString();
 
 var Client = require("./clients");
 var User = require("./users");
-
-function Device(user, key){
-    this.user=user;
-    this.key=key;
-
-    this.insertDevice = function(fn){
-        pg.connect(conString, function(err, client, done) {
-
-            if(err) {
-                console.log(err);
-                done();
-                return fn(err);
-            }
-
-            client.query("INSERT INTO _device (_key, _user) VALUES($1, $2)", [user, key],
-                function(err)
-                {
-                    if(err) {
-                        console.log(err);
-                        done();
-                        return fn(err);
-                    }
-                    done();
-                    return fn(null);
-                }
-            );
-        });
-    }
-
-    this.deleteDevice = function(fn) {
-
-        pg.connect(conString, function(err, client, done) {
-
-            if(err) {
-                console.log(err);
-                done();
-                return fn(err);
-            }
-
-            client.query("DELETE FROM _device WHERE _key=$1", [key],
-                function(err)
-                {
-                    if(err) {
-                        console.log(err);
-                        done();
-                        return fn(err);
-                    }
-                    done();
-                    return fn(null);
-                }
-            );
-        });
-    }
-}
+var Device = require("./devices");
 
 module.exports.pg = pg;
 module.exports.conString = conString;
 module.exports.Client = Client;
 module.exports.User = User;
-//module.exports.device = Device;
+module.exports.Device = Device;
