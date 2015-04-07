7a3916d321efafe80f05e97cbd57b709d8b3cfef
OksanaDizdari <Oksana.Dizdari@gmail.com>
2015-04-07 11:05:37 +0100

    Added notification entity to database

--- ServerSide_PushNotificationsWithGCM/DatabaseScript/CreateTables.sql
+++ ServerSide_PushNotificationsWithGCM/DatabaseScript/CreateTables.sql
@@ -1,4 +1,5 @@
-﻿DROP TABLE IF EXISTS _device;
+﻿DROP TABLE IF EXISTS _notification;
+DROP TABLE IF EXISTS _device;
 DROP TABLE IF EXISTS _user;
 DROP TABLE IF EXISTS _client;
 
@@ -26,4 +27,18 @@ CREATE TABLE _device
   CONSTRAINT _device_pkey PRIMARY KEY (_key,_user,_client),
   CONSTRAINT _user FOREIGN KEY (_user, _client)
   REFERENCES _user(_identifier, _client) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE _notification
+(
+ _key serial NOT NULL,
+ _user varchar(20) NOT NULL,
+ _client character varying(120) NOT NULL,
+ _device integer NOT NULL,
+ _delivered boolean DEFAULT false,
+  CONSTRAINT _notification_pkey PRIMARY KEY (_key, _client),
+  CONSTRAINT _user FOREIGN KEY (_user, _client)
+  REFERENCES _user(_identifier, _client) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT _device FOREIGN KEY (_device, _user, _client)
+  REFERENCES _device(_key,_user,_client) ON UPDATE CASCADE ON DELETE CASCADE
 );
